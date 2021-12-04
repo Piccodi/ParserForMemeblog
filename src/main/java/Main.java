e909854d7ac95fcd5b61eafe8a05290296b796e4
piccodi <akakoa@yandex.ru>
2021-12-04 20:28:37 +0300

    initial commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,3 @@
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class Main {
     public static void main(String[] args) throws InterruptedException {
